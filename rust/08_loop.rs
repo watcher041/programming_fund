74808d52e01a0d7cea989c484f19b495fc4892e3
watcher041 <watcher041@gmail.com>
2024-10-26 21:12:51 +0900

    while文を追記

--- rust/08_loop.rs
+++ rust/08_loop.rs
@@ -3,11 +3,20 @@
 fn main()
 {
     let mut number = 1;
+
+    // numberが10を超えればループを終了する
     loop{
-        println!("今{}回目です",number);
+        println!("loop：今{}回目です",number);
         number += 1; // number = number + 1
         if number > 10 {
             break;
         }
+    }
+
+    // numberが10以下ならループを続ける
+    number = 1;
+    while number <= 10 {
+        println!("while：今{}回目です",number);
+        number += 1;
     }
 }
